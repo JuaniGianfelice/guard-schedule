ba7cca44dedbd811a71e4f65d4b211e9a0b5f6a1
JuaniGianfelice <juani.gianfelice@gmail.com>
2023-12-13 15:12:46 -0300

    feat(Atlas conection)

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ app.get('/users', async (req, res) => {
   }
 })
 
+
 const PORT = process.env.PORT ||  8000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
--- server/server2.js
+++ server/server2.js
@@ -6,14 +6,17 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 //routes
-app.get("/", (req, res) => {
+app.get("/users", async (req, res) => {
+  const client = 
   res.send("Hola, Bienvenido");
 });
 
 //mongodb conection
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.DATABASE_URL)
   .then(() => console.log("Conected to Atlas"))
   .catch((error) => console.error(error));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
