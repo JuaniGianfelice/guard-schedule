eeb9bfb4e73bc4e68676541e54ab87e9fb4ccc79
JuaniGianfelice <juani.gianfelice@gmail.com>
2024-01-19 11:29:01 -0300

    feat(summary.scss)

--- client/src/components/summary/summary.jsx
+++ client/src/components/summary/summary.jsx
@@ -1,12 +1,15 @@
 import DateTime from "react-datetime";
+import "./summary.scss";
 
 const Summary = () => {
     return (
-        <div>
-            <p>Seleccionar Periodo</p>
-            <DateTime dateFormat="DD-MM-YYYY" timeFormat={false} />
-            <DateTime dateFormat="DD-MM-YYYY" timeFormat={false} />
-            <button>Buscar</button>
+        <div className="dash">
+            <div className="container">
+                <p className="date-title">Seleccionar Periodo</p>
+                <DateTime dateFormat="DD-MM-YYYY" timeFormat={false} className="date-select"/>
+                <DateTime dateFormat="DD-MM-YYYY" timeFormat={false} className="date-select"/>
+                <button className="btn-search">Buscar</button>
+            </div>
         </div>
     )
 }
